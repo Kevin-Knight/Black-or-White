a2959dffccac4409fdb601c12b83546a5bec4108
RESCUER\Kevin <1456358205@qq.com>
2018-04-12 15:25:09 +0800

    添加UserRepos和RelationRepos内容

--- src/main/java/com/petrichor/ving/dao/RelationRepos.java
+++ src/main/java/com/petrichor/ving/dao/RelationRepos.java
@@ -1,4 +1,13 @@
 package com.petrichor.ving.dao;
 
-public interface RelationRepos {
+import com.petrichor.ving.domain.Relation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RelationRepos extends JpaRepository<Relation, String> {
+    List<Relation> findByUId(String UId);
+    List<Relation> findByUIdAndAId(String UId, String AId);
+    List<Relation> findByUIdAndPId(String UId, String PId);
+    List<Relation> findByUIdAndPIdAndAId(String UId, String AId, String PId);
 }
--- src/main/java/com/petrichor/ving/dao/UserRepos.java
+++ src/main/java/com/petrichor/ving/dao/UserRepos.java
@@ -1,4 +1,13 @@
 package com.petrichor.ving.dao;
 
-public interface UserRepos {
+import com.petrichor.ving.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepos extends JpaRepository<User,String> {
+    List<User> findByUId(String UId);
+    List<User> findByUName(String UName);
+    List<User> findByUGender(String UGender);
+    List<User> findByUAgeBetween(String low, String high);
 }
